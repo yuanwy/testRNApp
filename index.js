9e85a4f41cb579cab96a01e499ae472307dbe90c
yuanwy <>
2017-11-23 09:28:58 +0800

    入门基础 demo

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import { AppRegistry } from 'react-native';
 
-import App from './App';
-import testProps from  './testProps';
-import testState from  './testState';
-import testStyle from  './testStyle';
-import testSize from  './testSize';
-import testFlexbox from  './testFlexbox';
-import testTextInput from  './testTextInput';
+import App from './primary/App';
+import testProps from './primary/testProps';
+import testState from './primary/testState';
+import testStyle from './primary/testStyle';
+import testSize from './primary/testSize';
+import testFlexbox from './primary/testFlexbox';
+import testTextInput from './primary/testTextInput';
+import testScrollView from './primary/testScrollView';
+import testListView from './primary/testListView';
+import testNet from './primary/testNet';
 
 // 注册应用(registerComponent)后才能正确渲染
 // 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
-AppRegistry.registerComponent('testRNApp', () => testTextInput);
+AppRegistry.registerComponent('testRNApp', () => testNet);
---
+++ primary/testNet.js
@@ -0,0 +1,24 @@
+/**
+ * Description: A brief description of the Com.
+ * Author: yuanwy
+ * Date: 23/11/2017
+ */
+
+import React, {Component} from 'react';
+
+import {
+    Text,
+} from 'react-native';
+
+
+class testNet extends Component<{}> {
+    render() {
+        return (
+            <Text>testNet!</Text>
+        );
+    }
+}
+
+export default testNet
+
+
